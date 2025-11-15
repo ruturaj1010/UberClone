1d3dee6a42965c0b2ff9b41a88d2f36a5f415381
Ruturaj <ruturajnikam1010@gmail.com>
2025-11-16 01:56:29 +0530

    created a Riding page for user

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -11,6 +11,7 @@ import UserProtectRoute from "./pages/UserProtectRoute";
 import UserLogout from "./pages/UserLogout";
 import CaptainHome from "./pages/CaptainHome";
 import CaptainProtectRoute from "./pages/CaptainProtectRoute";
+import Riding from "./pages/Riding";
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
         <Route path="/" element={<Start />} />
         <Route path="/signup" element={<UserSignup />} />
         <Route path="/login" element={<UserLogin />} />
+        <Route path="/riding" element={<Riding />} />
         <Route path="/captain-signup" element={<CaptainSignup />} />
         <Route path="/captain-login" element={<CaptainLogin />} />
         <Route
---
+++ Frontend/src/pages/Riding.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Riding = () => {
+  return (
+    <div className="w-screen h-screen">
+      <Link to="/home" className="fixed top-0 right-0 w-10 h-10 m-2 rounded-full bg-gray-300 flex items-center justify-center shadow-md z-10">
+        <i className="ri-home-5-line font-semibold text-xl"></i>
+      </Link>
+      <div className="h-1/2">
+        <img
+          className="h-full w-full object-cover object-center"
+          src="https://media.geeksforgeeks.org/wp-content/uploads/20220218205322/WhatsAppImage20220218at54912PM-304x660.jpeg"
+          alt="map img"
+        />
+      </div>
+      <div className="w-screen h-1/2 overflow-y-auto p-4">
+        <div className="flex items-center justify-between p-4">
+          <img
+            className="h-12 w-12 rounded-full object-cover object-center"
+            src="https://www.jeancoutu.com/globalassets/revamp/photo/conseils-photo/20160302-01-reseaux-sociaux-profil/photo-profil_301783868.jpg"
+            alt="img"
+          />
+          <div className="flex flex-col justify-center items-end font-semibold text-base">
+            <h2>Ruturaj</h2>
+            <h4 className="-my-1">MH10 AJ 5228</h4>
+            <p className="text-xs">Maruti Suzuki Baleno</p>
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-2 justify-start items-center">
+          <div className="w-full flex flex-col gap-4">
+            <div className="flex justify-start items-center gap-4 px-4 py-1">
+              <i className="ri-square-fill"></i>
+              <div>
+                <h3 className="font-semibold">Third wave coffee</h3>
+                <p className="text-sm text-gray-600">
+                  Shop No. 5, Near City Pride, Kolhapur
+                </p>
+              </div>
+            </div>
+
+            <div className="flex justify-start items-center gap-4 px-4 mb-5">
+              <i className="ri-bank-card-2-fill"></i>
+              <div>
+                <h3 className="font-semibold">₹193</h3>
+                <p className="text-sm text-gray-600">Cash - cash</p>
+              </div>
+            </div>
+          </div>
+          <button className="bg-green-500 text-white font-semibold text-xl border rounded-lg w-[95%] text-center py-1 mx-2">
+            Make a payment
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Riding;
