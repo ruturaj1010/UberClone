f79b77c423b62706341fbd15fd61dcb56310156c
Ruturaj <ruturajnikam1010@gmail.com>
2025-11-24 20:56:36 +0530

    added markers for deployment

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 import UserContext from "./context/UserContext.jsx";
 import CaptainContext from "./context/CaptainContext.jsx";
 import SocketProvider from "./context/SocketContext.jsx"
+import "./leafletConfig";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
